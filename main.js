9a7f9a3afeeb76f729060948abedfb480d5435be
Caique <49538313+caiquegiovannini@users.noreply.github.com>
2026-03-05 22:02:45 -0300

    Add browser worker validation

--- main.js
+++ main.js
@@ -3,17 +3,21 @@ const list = document.getElementById("list");
 const fpsCounter = document.getElementById("fps-counter");
 const totalEl = document.getElementById("total");
 
-const myWorker = new Worker('worker.js', { name: 'Heavy calculation fancy worker' });
-
-startButton.addEventListener("click", () => {
-    totalEl.innerText = 'Total: calculando em segundo plano...';
-    myWorker.postMessage(0);
-});
-
-myWorker.onmessage = (event) => {
-    const wrokerResult = event.data;
-    const total = wrokerResult;
-    totalEl.innerText = `Total: ${total}`;
+if (!window.Worker) {
+    console.error("Your browser doesn't support web workers.");
+} else {
+    const myWorker = new Worker('worker.js', { name: 'Heavy calculation fancy worker' });
+    
+    startButton.addEventListener("click", () => {
+        totalEl.innerText = 'Total: calculando em segundo plano...';
+        myWorker.postMessage(0);
+    });
+    
+    myWorker.onmessage = (event) => {
+        const wrokerResult = event.data;
+        const total = wrokerResult;
+        totalEl.innerText = `Total: ${total}`;
+    };
 };
 
 addEventListener("mousedown", () => {
